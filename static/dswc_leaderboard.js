c0158bd13bbd316a550a5fe705e3e085dc23ad27
Albert Peiro <acordonet@gmail.com>
2020-01-30 00:18:50 +0000

    fix: single quotes

--- static/dswc_leaderboard.js
+++ static/dswc_leaderboard.js
@@ -1,6 +1,6 @@
-Vue.prototype.$http = "axios";
+Vue.prototype.$http = 'axios';
 
-var dataSource = "";
+var dataSource = '';
 
 /* If the api engine is not running locally, to do development on:
 
@@ -11,17 +11,17 @@ var dataSource = "";
    but the below code will switch the API source to Heroku */
 
 if (window.location.href.search(/dev.html/) != -1) {
-    dataSource = "https://dswcregatta.herokuapp.com";
+    dataSource = 'https://dswcregatta.herokuapp.com';
 }
 
 function mkDisplayData(obj, data) {
     var dData = [];
     var fields = [
-        { key: "crew", stickyColumn: true },
-        { key: "place", class: "text-center" },
+        { key: 'crew', stickyColumn: true },
+        { key: 'place', class: 'text-center' },
         /* {'key': 'boatNum', class: 'text-center'}, */
 
-        { key: "points", class: "text-center" }
+        { key: 'points', class: 'text-center' }
     ];
     for (var result of data) {
         var dRec = {};
@@ -31,39 +31,39 @@ function mkDisplayData(obj, data) {
         dRec.points = result.points;
         dRec.crew =
             (result.place === 1
-                ? "🥇"
+                ? '🥇'
                 : result.place === 2
-                ? "🥈"
+                ? '🥈'
                 : result.place === 3
-                ? "🥉"
-                : "") + result.crew.join(", ");
+                ? '🥉'
+                : '') + result.crew.join(', ');
 
         // First, second and third positions
         var highlightResult =
             result.place === 1
-                ? "primary"
+                ? 'primary'
                 : result.place === 2 || result.place === 3
-                ? "success"
-                : "";
+                ? 'success'
+                : '';
         dRec._cellVariants = { place: highlightResult };
 
         for (var r in result.races) {
-            var raceStr = "R" + parseInt(r, 10) + 1;
+            var raceStr = 'R' + parseInt(r, 10) + 1;
             var fRec = {
                 key: raceStr,
-                class: "text-center"
+                class: 'text-center'
             };
             fields.push(fRec);
 
             dRec[raceStr] = result.races[r].place;
 
             if (result.races[r].discard) {
-                dRec[raceStr] = "(" + dRec[raceStr] + ")";
-                dRec._cellVariants[raceStr] = "danger";
+                dRec[raceStr] = '(' + dRec[raceStr] + ')';
+                dRec._cellVariants[raceStr] = 'danger';
             } else if (result.races[r].flag) {
-                dRec._cellVariants[raceStr] = "warning";
+                dRec._cellVariants[raceStr] = 'warning';
             } else {
-                dRec._cellVariants[raceStr] = "info";
+                dRec._cellVariants[raceStr] = 'info';
             }
         }
         dData.push(dRec);
@@ -73,19 +73,19 @@ function mkDisplayData(obj, data) {
 }
 
 var allSeries = new Vue({
-    el: "#allSeries",
+    el: '#allSeries',
     data: {
         seriesList: []
     },
     mounted() {
         axios
-            .get(dataSource + "/api/v1.0/listSeries")
+            .get(dataSource + '/api/v1.0/listSeries')
             .then(response => (this.seriesList = response.data));
     }
 });
 
-Vue.component("result-table", {
-    props: ["title", "tabledata"],
+Vue.component('result-table', {
+    props: ['title', 'tabledata'],
     template: `
         <div class='result-table'>
             <h3>Results for <span class="text-primary">{{ title.split('_')[0] }} {{ title.split('_')[1] }}</span></h3>
@@ -97,7 +97,7 @@ Vue.component("result-table", {
 });
 
 var resultsPane = new Vue({
-    el: "#resultsPane",
+    el: '#resultsPane',
     data: {
         showResults: false,
         summary: null,
@@ -105,7 +105,7 @@ var resultsPane = new Vue({
         summaryTitle: null,
         summaryData: {
             items: null,
-            fields: ["crew", "place", "boatNum", "points", "races"]
+            fields: ['crew', 'place', 'boatNum', 'points', 'races']
         }
     },
     // mounted () {
@@ -121,7 +121,7 @@ var resultsPane = new Vue({
                 .get(
                     dataSource +
                         encodeURI(
-                            "/api/v1.0/getSeriesResult?seriesName=" + series
+                            '/api/v1.0/getSeriesResult?seriesName=' + series
                         )
                 )
                 .then(response => mkDisplayData(this, response.data));
